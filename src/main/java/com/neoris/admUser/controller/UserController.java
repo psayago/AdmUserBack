4ebb38ebc68c196ccb4ec7cb9575e9920e791f01
psayago <psayago@est.frba.utn.edu.ar>
2020-03-27 20:49:44 -0300

    Creado restcontroller y repository

--- src/main/java/com/neoris/admUser/controller/UserController.java
+++ src/main/java/com/neoris/admUser/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.neoris.admUser.controller;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.gson.Gson;
+import com.neoris.admUser.model.RestResponse;
 import com.neoris.admUser.model.User;
 import com.neoris.admUser.repositories.UserRepository;
 
@@ -32,28 +39,39 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "api/authenticate/login", method = RequestMethod.POST)
-	public ResponseEntity<String> saveUser(@RequestBody User user) {
-		HttpHeaders responseHeaders = new HttpHeaders();
+	//@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+	public ResponseEntity<User> saveUser(@RequestBody User user) {
+//		HttpHeaders responseHeaders = new HttpHeaders();
 		
-		if (userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword()) != null) {
-			responseHeaders.add("Request Method", "POST");
-			responseHeaders.add("Status Code", "200");
-			responseHeaders.add("Content-Type","application/json");
-			Gson gson = new Gson();
-			String json = gson.toJson(user);
-			return ResponseEntity.ok()
-				      .headers(responseHeaders)
-				      .body( json);
-		} else {
-			responseHeaders.add("Request Method", "POST");
-		    responseHeaders.add("Status Code", "400");
-		    responseHeaders.add("Content-Type","application/json");
-		    System.out.println(responseHeaders);
-			return ResponseEntity.badRequest()
-				      .headers(responseHeaders)
-				      .body("{}");
+		User usuario = new User();
+		
+		try {
+			usuario = userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
+			return new ResponseEntity<User>(usuario, HttpStatus.OK);
+			
+		} catch (Exception e) {
+			return new ResponseEntity<User>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-	 
+		
+		
+//			responseHeaders.add("Request Method", "POST");
+//			responseHeaders.add("Status Code", "200");
+//			responseHeaders.add("Content-Type","application/json");
+//			Gson gson = new Gson();
+//			String json = gson.toJson(user);
+//			System.out.println("entro en trueee");
+//			return ResponseEntity.ok()
+//				      .headers(responseHeaders)
+//				      .body( json);
+//			
+//		} else {
+//			responseHeaders.add("Request Method", "POST");
+//		    responseHeaders.add("Status Code", "400");
+//		    responseHeaders.add("Content-Type","application/json");
+//		    System.out.println(responseHeaders);
+//			return ResponseEntity.badRequest()
+//				      .headers(responseHeaders)
+//				      .body("{}");
 	    
 	}
 
